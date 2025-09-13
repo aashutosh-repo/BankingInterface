cbeecc17fc4e12567bb0d1ab92577c07abf3c384
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2025-09-14 00:22:17 +0530

    feat: Implement PaymentDetailsComponent with status display and routing integration

--- src/app/routes/payment.routes.ts
+++ src/app/routes/payment.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PaymentProcesingComponent } from '../pages/payments/payment-procesing/payment-procesing.component';
 import { BulkPaymentsComponent } from '../pages/payments/bulk-payments/bulk-payments.component';
 import { LayoutComponent } from '../shared/layout/layout/layout.component';
+import { PaymentDetailsComponent } from '../pages/payments/payment-details/payment-details.component';
 
 export const paymentRoutes: Routes = [
   {
@@ -9,7 +10,8 @@ export const paymentRoutes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'digital', component: PaymentProcesingComponent },
-      { path: 'bulk-payments', component: BulkPaymentsComponent }
+      { path: 'bulk-payments', component: BulkPaymentsComponent },
+      { path : 'success', component: PaymentDetailsComponent }
     ]
   }
 ];
