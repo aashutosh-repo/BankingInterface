7b0a5ee16ca2cbd87cc1de63c307eda13df87842
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2025-09-22 01:23:22 +0530

    feat: Implement mobile navigation with hamburger menu and routing options

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -8,18 +8,23 @@ import { LoginComponent } from '../../../pages/login/login.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { UserResponse } from '../../../model/interfaces/security/UserDetails.model';
+import { MatOptionModule } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 
 
 @Component({
   selector: 'app-header',
   imports: [CommonModule, MatFormFieldModule, MatToolbarModule, MatButtonModule, 
-    MatIconModule, MatSidenavModule],
+    MatIconModule, MatSidenavModule, MatOptionModule, MatSelectModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog,
+              private router: Router
+  ) {}
   isExpanded = false;
   userDetails: UserResponse | null = null;
   showUserDetails: boolean = false;
@@ -45,4 +50,21 @@ export class HeaderComponent {
         }
       });
   }
+  onMenuSelect(event: MatSelectChange) {
+    const option = event.value;
+  switch (option) {
+    case 'home':
+      this.router.navigate(['/home']);
+      break;
+    case 'docs':
+      this.router.navigate(['/docs']);
+      break;
+    case 'shortcut':
+      this.router.navigate(['/shortcut']);
+      break;
+    case 'about':
+      this.router.navigate(['/about']);
+      break;
+  }
+}
 }
